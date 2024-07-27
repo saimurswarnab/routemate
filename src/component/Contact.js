3732c45649eb7d2619effe6fe6af9f0902539a84
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-07-28 02:36:56 +0600

    useNavigate react dom used

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,6 +1,18 @@
-
+import { useNavigate } from "react-router-dom";
 export const Contact = () => {
+
+  const navigate = useNavigate();
+
+  const handleSubmit = () =>{
+    console.log("--------------");
+    navigate("/");
+
+  }
   return (
-    <div>Contact</div>
+    <>
+     <div className="component">Contact</div>
+     <button onClick={handleSubmit}>Back to Home</button>
+    </>
+   
   )
 }
