6b0923d8990f66623fa8424753a49a376cda3c03
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-07-27 19:50:02 +0600

    Using link

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,15 +1,17 @@
+import { Link, NavLink } from "react-router-dom";
 import Logo from "../assets/logo.png";
 export const Header = () => {
   return (
     <header>
-      <a href="" className="logo">
+      <Link to="" className="logo">
         <img src={Logo} alt="RouteMate Logo"/>
         <span>Routemate</span>
-      </a>
+      </Link>
       <nav className="navigation">
-        <a href="/" className="link">Home</a>
-        <a href="/products" className="link">Products</a>
-        <a href="/contact" className="link">Contact</a>
+        
+        <Link to="/" className="link">Home</Link>
+        <Link to="/products" className="link">Products</Link>
+        <Link to="/contact" className="link">Contact</Link>
 
       </nav>
     </header>
