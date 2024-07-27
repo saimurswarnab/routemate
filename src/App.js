6dd458adfb211906cb9db2676565730367291764
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-07-27 18:36:00 +0600

    A base structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { Routes , Route} from "react-router-dom";
 import './App.css';
+import { ProductDetail } from './component/ProductDetail';
+import { ProductList } from './component/ProductList';
+import { Home } from './component/Home';
+import { Contact } from './component/Contact';
 function App() {
   return (
     
     <div className="App">
-      <h2>Hello World</h2>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="products" element={<ProductList/>}></Route>
+        <Route path="products/1001" element={<ProductDetail/>}></Route>
+        <Route path="contact" element={<Contact/>}></Route>
+
+      </Routes>
     </div>
 
   );
