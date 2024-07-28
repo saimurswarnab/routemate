1abea3d978bc546b2e02ac2115d13dd1f87f5a4e
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-07-29 04:08:12 +0600

    Add routes Restructuring

--- src/App.js
+++ src/App.js
@@ -1,34 +1,21 @@
-import { Routes , Route, Navigate} from "react-router-dom";
+import { AllRoutes } from './routes/AllRoutes';
 import './App.css';
 
-import { Home, Admin, ProductDetail, ProductList, Contact, ContactEu, ContactIn, ContactUs, PageNotFound } from './pages/index';
+
 
 import { Header,Footer } from './component/index';
 
 
 function App() {
-  const user = false;
+
   return (
     
     <div className="App">
       <Header/>
       <main>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="products" element={<ProductList/>}/>
-        <Route path="products/:id" element={<ProductDetail/>}/>
-        <Route path="contact" element={<Contact/>}>
-          <Route path="in" element={<ContactIn/>}/>
-          <Route path="eu" element={<ContactEu/>}/>
-          <Route path="us" element={<ContactUs/>}/>
-        </Route>
-        <Route path="contact/in" element={<Contact/>}/>
-        <Route path="contact/eu" element={<Contact/>}/>
-        <Route path="contact/us" element={<Contact/>}/>
-        <Route path="/admin" element={user ? <Admin/> :<Navigate to="/"/>}/>
-        <Route path="*" element={<PageNotFound/>}/>
 
-      </Routes>
+        <AllRoutes/>
+
 
 
       </main>
---
+++ src/routes/AllRoutes.js
@@ -0,0 +1,28 @@
+import { Routes , Route, Navigate} from "react-router-dom";
+import { Home, Admin, ProductDetail, ProductList, Contact, ContactEu, ContactIn, ContactUs, PageNotFound } from '../pages/index';
+export const AllRoutes = () => {
+
+  const user = false;
+  return (
+    <>
+    <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="products" element={<ProductList/>}/>
+        <Route path="products/:id" element={<ProductDetail/>}/>
+        <Route path="contact" element={<Contact/>}>
+          <Route path="in" element={<ContactIn/>}/>
+          <Route path="eu" element={<ContactEu/>}/>
+          <Route path="us" element={<ContactUs/>}/>
+        </Route>
+        <Route path="contact/in" element={<Contact/>}/>
+        <Route path="contact/eu" element={<Contact/>}/>
+        <Route path="contact/us" element={<Contact/>}/>
+        <Route path="/admin" element={user ? <Admin/> :<Navigate to="/"/>}/>
+        <Route path="*" element={<PageNotFound/>}/>
+
+    </Routes>
+
+
+    </>
+  )
+}
