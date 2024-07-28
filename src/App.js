13a8637fcc45dbba9d22fe533812833c89814d69
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-07-28 20:54:08 +0600

    access differant page

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { Footer } from './component/Footer';
 import { Admin } from "./component/Admin";
 import { PageNotFound } from "./component/PageNotFound";
 function App() {
-  const user = false;
+  const user = true;
   return (
     
     <div className="App">
@@ -20,7 +20,7 @@ function App() {
         <Route path="products" element={<ProductList/>}/>
         <Route path="products/:id" element={<ProductDetail/>}/>
         <Route path="contact" element={<Contact/>}/>
-        <Route path="/admin" element={user ? <Admin/> :<Navigate to="/"/>}/>
+        <Route path="/admin" element={user ? <Admin/> : <PageNotFound/>}/>
         <Route path="*" element={<PageNotFound/>}/>
 
       </Routes>
