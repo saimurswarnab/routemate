27ac5014a756b9fcd33f5520f9b49028dcdb8d3e
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-07-28 20:35:25 +0600

    uSeloaction

--- src/component/ProductList.js
+++ src/component/ProductList.js
@@ -1,9 +1,12 @@
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams,useLocation } from "react-router-dom";
 export const ProductList = () => {
   const [searchParams] = useSearchParams();
-  console.log(searchParams.get("keyword"));
-  console.log(searchParams.get("instock"));
-  console.log(searchParams.get("rating"));
+  // console.log(searchParams.get("keyword"));
+  // console.log(searchParams.get("instock"));
+  // console.log(searchParams.get("rating"));
+  const location = useLocation();
+  console.log(location);
+
   return (
     <div className="component">ProductList</div>
   )
