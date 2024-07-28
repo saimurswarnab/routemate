f4b9c519c825938542d1103390dee6a63fff5a1b
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-07-28 20:31:04 +0600

    Use search Params

--- src/component/ProductList.js
+++ src/component/ProductList.js
@@ -1,5 +1,9 @@
-
+import { useSearchParams } from "react-router-dom";
 export const ProductList = () => {
+  const [searchParams] = useSearchParams();
+  console.log(searchParams.get("keyword"));
+  console.log(searchParams.get("instock"));
+  console.log(searchParams.get("rating"));
   return (
     <div className="component">ProductList</div>
   )
