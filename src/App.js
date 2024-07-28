c555b344cc90de6a573c85fc363574e3b8cf7a1f
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-07-28 21:48:59 +0600

    Using nested route in contract

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import { Header } from './component/Header';
 import { Footer } from './component/Footer';
 import { Admin } from "./component/Admin";
 import { PageNotFound } from "./component/PageNotFound";
+import { ContactEu } from "./component/ContactEu";
+import { ContactUs } from './component/ContactUs';
+import { ContactIn } from './component/ContactIn';
 function App() {
   const user = false;
   return (
@@ -19,7 +22,14 @@ function App() {
         <Route path="/" element={<Home/>}/>
         <Route path="products" element={<ProductList/>}/>
         <Route path="products/:id" element={<ProductDetail/>}/>
-        <Route path="contact" element={<Contact/>}/>
+        <Route path="contact" element={<Contact/>}>
+          <Route path="in" element={<ContactIn/>}/>
+          <Route path="eu" element={<ContactEu/>}/>
+          <Route path="us" element={<ContactUs/>}/>
+        </Route>
+        <Route path="contact/in" element={<Contact/>}/>
+        <Route path="contact/eu" element={<Contact/>}/>
+        <Route path="contact/us" element={<Contact/>}/>
         <Route path="/admin" element={user ? <Admin/> :<Navigate to="/"/>}/>
         <Route path="*" element={<PageNotFound/>}/>
 
